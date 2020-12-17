3169d05f7168d004e6d800e75bc6439de9824bf3
WHU-Traffic-Sign-Detection-Team <Ma_Yaowei@outlook.com>
2020-12-17 12:54:54 +0800

    draw satellite - 2 - draw satellites per second

--- starlink/src/components/WorldMap.js
+++ starlink/src/components/WorldMap.js
@@ -5,6 +5,9 @@ import {feature} from "topojson-client"
 import { geoKavrayskiy7 } from 'd3-geo-projection';
 import { geoGraticule, geoPath } from 'd3-geo';
 import { select as d3Select } from 'd3-selection';
+import { timeFormat as d3TimeFormat } from "d3-time-format";
+import { schemeCategory10 } from "d3-scale-chromatic";
+import * as d3Scale from "d3-scale";
 
 import {WORLD_MAP_URL, SATELLITE_POSITION_URL, SAT_API_KEY} from "../constants"
 
@@ -15,10 +18,11 @@ class WorldMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      map: null
     }
     this.refMap = React.createRef();                                  // use ref to get a virtual node. So as we can draw on it.
     this.refTrack = React.createRef();
+    this.map = null;
+    this.color = d3Scale.scaleOrdinal(schemeCategory10); // connect color and number
   }
 
   componentDidMount() {
@@ -59,12 +63,72 @@ class WorldMap extends Component {
     }
   }
 
-  // a callback used in componentDidUpdate - draw satellites on the second canvas.
+  // a callback used in componentDidUpdate - draw all satellites on the second canvas.
   track = data => {
-    console.log(data)
+    // validate the input
+    if (!data || !data[0].hasOwnProperty('positions')) {
+      throw new Error("no position data", data);
+      return;
+    }
+
+    const len = data[0].positions.length;
+    const { duration } = this.props.settings;
+    const { context2 } = this.map;
+
+    // Step 1: draw time on the top
+    // current time
+    let initialTime = new Date();
+    let i = 0;
+    // interval between two drawing
+    let timer = setInterval(() => {
+      let currentTime = new Date();
+      let timePassed = i === 0 ? 0 : currentTime - initialTime;
+      // 1 sec in display == 1 minute in real world (corresponding to accelerated positions we got)
+      let time = new Date(initialTime.getTime() + 60 * timePassed);
+
+      context2.clearRect(0, 0, width, height);
+
+      context2.font = "bold 20px sans-serif";
+      context2.fillStyle = "#333";
+      context2.textAlign = "center";
+      context2.fillText(d3TimeFormat(time), width / 2, 20);
+
+      if (i >= len) {                   // timer cannot excced the length of positions array
+        clearInterval(timer);
+        return;
+      }
+
+      data.forEach(sat => {             // draw
+        const { info, positions } = sat;
+        this.drawSat(info, positions[i]);
+      });
+
+      i += 60;
+    }, 1000)
+
   }
 
-  // a callback used in componentDidMount - draw a world map on the first canvas
+  // a helper function used in track to draw each satellite
+  drawSat = (sat, pos) => {
+    const { satlongitude, satlatitude } = pos;
+    if (!satlongitude || !satlongitude) return;
+
+    const { satname } = sat;
+    const nameWithNumber = satname.match(/\d+/g).join(""); // extract satname number
+
+    const { projection, context2 } = this.map;
+    const xy = projection([satlongitude, satlatitude]); // get coordinate
+    context2.fillStyle = this.color(nameWithNumber); // name is a number
+    context2.beginPath();
+    context2.arc(xy[0], xy[1], 4, 0,2 * Math.PI); //radius, startAngle, endAngle
+    context2.fill();
+
+    context2.font = "bold 11px sans-serif";
+    context2.textAlign = "center";
+    context2.fillText(nameWithNumber, xy[0], xy[1] + 14);
+  }
+
+  // a callback used in componentDidMount - prepare 2 canvases' context & draw a world map on the first canvas
   generateMap = land => {
     const projection = geoKavrayskiy7()                            // Step 1: configure the projection method
       .scale(180)
@@ -74,11 +138,17 @@ class WorldMap extends Component {
 
     const graticule = geoGraticule();                              // Step 2: get the longitude and latitude
 
-    const canvas = d3Select(this.refMap.current)                  // Step 3: let d3-selection get the canvas to draw
+    const canvas = d3Select(this.refMap.current)                  // Step 3.1: let d3-selection get the canvas to draw world map
+      .attr("width", width)
+      .attr("height", height);
+
+    const canvas2 = d3Select(this.refTrack.current)               // Step 3.2: let d3 get the canvas to draw the satellite
       .attr("width", width)
       .attr("height", height);
 
     const context = canvas.node().getContext("2d");       // path can convert realworld data to context using projection
+    const context2 = canvas2.node().getContext("2d");
+
     let path = geoPath().projection(projection).context(context);
 
     land.forEach( ele => {                                        // Step 4.1 : draw countries' border
@@ -101,6 +171,12 @@ class WorldMap extends Component {
     context.lineWidth = 1;
     path(graticule.outline());
     context.stroke();
+
+    this.map = {                                                //Step 5: store map in a field. they don't update UI so
+      context: context,                                          //       there is no need to store them in a state
+      context2: context2,
+      projection: projection
+    }
   }
 
   render() {
